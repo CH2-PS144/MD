4fb9dd86e4c6421761274eae15112187f259c38f
r <rneear@gmail.com>
2023-12-12 04:25:21 +0700

    Modify: Modifikasi Fragment di homeFragment

--- app/src/main/java/com/example/physedu/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/physedu/fragment/HomeFragment.kt
@@ -9,55 +9,33 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.physedu.R
+import com.example.physedu.activity.ClassObject
 import com.example.physedu.activity.KelasUser
 import com.example.physedu.adapter.KelasUserAdapter
 import com.example.physedu.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private lateinit var rvkelasUser: RecyclerView
-    private val list = ArrayList<KelasUser>()
+    private lateinit var rvKelasUser: RecyclerView
+    private var list: ArrayList<KelasUser> = arrayListOf()
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-////        list.addAll(getListKelas())
-////        showRecyclerList()
-//    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-        rvkelasUser = view.findViewById(R.id.rv_kelasUser) ?: throw IllegalStateException("RecyclerView not found")
+        rvKelasUser = view.findViewById(R.id.rv_kelasUser)
+        rvKelasUser.setHasFixedSize(true)
 
-        rvkelasUser.setHasFixedSize(true)
-        list.addAll(getListKelas())
-
+        list.addAll(ClassObject.listDataKelas)
         showRecyclerList()
+
         return view
     }
 
-    private fun getListKelas(): ArrayList<KelasUser> {
-        val dataKelas = resources.getStringArray(R.array.kelas)
-        val dataMulai = resources.getStringArray(R.array.mulai)
-        val dataGambar = resources.obtainTypedArray(R.array.gambar)
-        val listKelas = ArrayList<KelasUser>()
-        for (i in dataKelas.indices) {
-            val kelas = KelasUser(dataKelas[i], dataMulai[i], dataGambar.getResourceId(i, -1))
-            listKelas.add(kelas)
-        }
-        return listKelas
-    }
-
     private fun showRecyclerList() {
-        rvkelasUser.layoutManager = LinearLayoutManager(this.context)
-        val listKelasAdapter = KelasUserAdapter(list)
-        rvkelasUser.adapter = listKelasAdapter
-//
-//        listHeroAdapter.setOnItemClickCallback(object : KelasUserAdapter.OnItemClickCallback {
-//            override fun onItemClicked(data: KelasUser) {
-//                showSelectedCabai(data)
-//            }
-//        })
+        rvKelasUser.layoutManager = LinearLayoutManager(context)
+        val listHeroAdapter = KelasUserAdapter(list)
+        rvKelasUser.adapter = listHeroAdapter
     }
-
 }
