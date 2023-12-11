c3fda4a769841d1c2d51222680df3d9155e5bef3
r <rneear@gmail.com>
2023-12-12 04:18:55 +0700

    modify: Modifikasi Activity pada Kelas User dan Main Activity

---
+++ app/src/main/java/com/example/physedu/activity/ClassObject.kt
@@ -0,0 +1,37 @@
+package com.example.physedu.activity
+
+import com.example.physedu.R
+
+object ClassObject {
+
+    private val nameClass = arrayOf(
+        "KELAS VII",
+        "KELAS VIII",
+        "KELAS IX"
+    )
+
+    private val textMulai = arrayOf(
+        "Ayo Mulai",
+        "Ayo Mulai",
+        "Ayo Mulai"
+    )
+
+    private val avatar = intArrayOf(
+        R.drawable.icon_kelas_1,
+        R.drawable.icon_kelas_2,
+        R.drawable.icon_kelas_3
+    )
+
+    val listDataKelas: ArrayList<KelasUser>
+        get() {
+            val list = arrayListOf<KelasUser>()
+            for (position in nameClass.indices ) {
+                val grade = KelasUser()
+                grade.kelas = nameClass[position]
+                grade.mulai = textMulai[position]
+                grade.gambar = avatar[position]
+                list.add(grade)
+            }
+            return list
+        }
+}
--- app/src/main/java/com/example/physedu/activity/MainActivity.kt
+++ app/src/main/java/com/example/physedu/activity/MainActivity.kt
@@ -1,53 +1,44 @@
 package com.example.physedu.activity
 
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.physedu.R
-import com.example.physedu.databinding.ActivityMainBinding
 import com.example.physedu.fragment.AboutFragment
 import com.example.physedu.fragment.HomeFragment
 import com.example.physedu.fragment.KameraFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bottomNavigationView: BottomNavigationView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottomNavigationView = findViewById(R.id.nav_view)
-        bottomNavigationView.setOnItemSelectedListener {menuItem ->
-            when(menuItem.itemId){
-                R.id.navigation_home -> {
-                    replaceFragment(HomeFragment())
-                    return@setOnItemSelectedListener true
-                }
-                R.id.navigation_kamera -> {
-                    replaceFragment(KameraFragment())
-                    return@setOnItemSelectedListener true
-                }
-                R.id.navigation_about -> {
-                    replaceFragment(AboutFragment())
-                    return@setOnItemSelectedListener true
-                }
+        val fragmentManager = supportFragmentManager
+        val homeFragment = HomeFragment()
+        val cameraFragment = KameraFragment()
+        val aboutFragment = AboutFragment()
+
+        makeCurrentFragment(homeFragment)
+        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        val bottom_navigation = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_home -> makeCurrentFragment(homeFragment)
+                R.id.navigation_kamera -> makeCurrentFragment(cameraFragment)
+                R.id.navigation_about -> makeCurrentFragment(aboutFragment)
             }
             true
         }
-
     }
 
-    private fun replaceFragment(fragment: Fragment) {
+    private fun makeCurrentFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.fl_fragment, fragment)
+            addToBackStack(null)
             commit()
         }
     }
-
 }
