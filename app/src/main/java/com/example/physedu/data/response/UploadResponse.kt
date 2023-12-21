49d809d5e827a28809b0160afd017e2d2e4c5e82
r <rneear@gmail.com>
2023-12-22 03:55:44 +0700

    Remove Nullable

--- app/src/main/java/com/example/physedu/data/response/UploadResponse.kt
+++ app/src/main/java/com/example/physedu/data/response/UploadResponse.kt
@@ -1,12 +1,12 @@
-package com.example.physedu.response
+package com.example.physedu.data.response
 
 import com.google.gson.annotations.SerializedName
 
 data class UploadResponse(
 
 	@field:SerializedName("result")
-	val result: String? = null,
+	val result: String,
 
 	@field:SerializedName("question")
-	val question: String? = null
+	val question: String
 )
