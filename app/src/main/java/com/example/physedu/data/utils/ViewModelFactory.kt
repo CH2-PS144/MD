867b284715c78c26a5556fe319d740bae28824fb
r <rneear@gmail.com>
2023-12-22 03:56:50 +0700

    Update: Pindah Package

--- app/src/main/java/com/example/physedu/data/utils/ViewModelFactory.kt
+++ app/src/main/java/com/example/physedu/data/utils/ViewModelFactory.kt
@@ -1,9 +1,11 @@
-package com.example.physedu
+package com.example.physedu.data.utils
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.physedu.activity.ResultViewModel
+import com.example.physedu.data.Injection
+import com.example.physedu.data.repos.UserRepository
+import com.example.physedu.ui.result.ResultViewModel
 
 class ViewModelFactory(private val repository: UserRepository) : ViewModelProvider.NewInstanceFactory() {
 
