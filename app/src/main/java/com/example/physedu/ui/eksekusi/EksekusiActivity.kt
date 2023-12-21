a4e6b62b57b272e051e24a493f1ab20d8ca2fdce
r <rneear@gmail.com>
2023-12-22 03:48:10 +0700

    Update: Pembahan Variable Companion Object dan Set Hasil dari Response Model ke Dalam TextView

--- app/src/main/java/com/example/physedu/ui/eksekusi/EksekusiActivity.kt
+++ app/src/main/java/com/example/physedu/ui/eksekusi/EksekusiActivity.kt
@@ -1,19 +1,23 @@
-package com.example.physedu
+package com.example.physedu.ui.eksekusi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.physedu.activity.ResultActivity.Companion.QUESTION
-import com.example.physedu.activity.ResultActivity.Companion.RESULT
 import com.example.physedu.databinding.ActivityEksekusiBinding
 
 class EksekusiActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityEksekusiBinding
+    private lateinit var binding: ActivityEksekusiBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEksekusiBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.hasilSoal.text = QUESTION
-        binding.hasilJawaban.text = RESULT
+        binding.tvQuest.text = intent.getStringExtra(EXTRA_QUESTION)
+        binding.tvResult.text = intent.getStringExtra(EXTRA_RESULT)
+    }
+
+    companion object {
+         const val EXTRA_QUESTION = "extra_question"
+         const val EXTRA_RESULT = "extra_result"
     }
 }
